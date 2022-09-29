12a78e907c82e29619ce65621a413906e8214526
Bezdelnik2021 <548gggg@gmail.com>
2022-09-29 21:31:22 +0300

    Add task 5

--- sum_in_array.py
+++ sum_in_array.py
@@ -8,3 +8,23 @@ for Sierentz Global Merchants
 
 Test task
 """
+
+
+def sum_in_array(list, s):
+    result = [-1]
+
+    i = 0
+    j = len(list) - 1
+
+    while j > i:
+        if list[i] + list[j] > s:
+            j -= 1
+        elif list[i] + list[j] < s:
+            i += 1
+        else:
+            result = [list[i], list[j]]
+            break
+
+    return result
+
+# print(sum_in_array([-3,1,4,6], 7))
