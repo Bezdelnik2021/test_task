1769fcded13a36a64f86fbff8c6194e28d237898
Bezdelnik2021 <548gggg@gmail.com>
2022-09-29 21:17:36 +0300

    Add task 3

--- lucky.py
+++ lucky.py
@@ -8,3 +8,23 @@ for Sierentz Global Merchants
 
 Test task
 """
+
+
+def lucky_series(num):
+    result = []
+    while num > 0:
+        result.append(num % 10)
+        num //= 10
+
+    max_series = [0]
+    temp_series = []
+
+    for i in result:
+        if i in [5, 6]:
+            temp_series.append(i)
+            if len(max_series) < len(temp_series) and len(set(temp_series)) > 1:
+                max_series = temp_series
+        else:
+            temp_series = []
+
+    return sum(n * 10 ** i for i, n in enumerate(max_series))
